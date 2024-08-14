10176caa67ecfaac8355a5cc52ef56f1959d571a
Челединов Станислав Сергеевич <ext-cheledinov_s@bi.group>
2024-08-14 23:38:42 +0600

    first commit

---
+++ src/pages/MainPage/components/SideBar/SideBar.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./Navbar";
+import YourCollections from "./YourCollections";
+
+const SideBar = () => {
+  return (
+    <>
+      <NavBar/>
+      <YourCollections/>
+    </>
+  )
+}
+
+export default SideBar;
---
+++ src/pages/MainPage/components/SideBar/YourCollections/YourCollections.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const YourCollections = () => {
+  return (
+    <div>YourCollections</div>
+  )
+}
+
+export default YourCollections
